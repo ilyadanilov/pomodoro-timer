5245ad0f7f218cb06462bcc3602a1083e29a6549
Ilya Danilov <danilov.freelance@gmail.com>
2023-06-26 00:15:34 +0300

    Added long break after 4 pomodoros

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var startBtn = document.querySelector(".pomodoro__start-btn");
-var timer = document.querySelector(".pomodoro__timer");
-var resetBtn = document.querySelector(".pomodoro__reset-btn");
-var defaultMinutes = 25;
-let breakMinutes = 5;
-var countdownInterval = null;
-var blinkInterval = null;
-var remainingTime = defaultMinutes * 60;
+// Пользовательские значения
+const userDefinedPomodoroLength = null;
+const userDefinedShortBreak = null;
+const userDefinedLongBreak = null;
+const userDefinedPomodorosTillLongBreak = null;
+// Дефолтные значения и селекторы
+const startBtn = document.querySelector(".pomodoro__start-btn");
+const timer = document.querySelector(".pomodoro__timer");
+const resetBtn = document.querySelector(".pomodoro__reset-btn");
+const defaultPomodoroLength = userDefinedPomodoroLength || 25;
+const shortBreak = userDefinedShortBreak || 5;
+const longBreak = userDefinedLongBreak || 15;
+const pomodorosTillLongBreak = userDefinedPomodorosTillLongBreak || 4;
+let countdownInterval = null;
+let blinkInterval = null;
+let remainingTime = defaultPomodoroLength * 60;
 let isStarted = false;
 let isBreak = false;
+let pomodoroCounter = 0;
 // FUNCTION countdown
 // -----------------------------------------------------
 function countdown() {
@@ -19,7 +28,18 @@ function countdown() {
     startBtn.innerHTML = "Start";
     isStarted = false;
     isBreak = !isBreak;
-    remainingTime = isBreak ? breakMinutes * 60 : defaultMinutes * 60;
+    if (isBreak) {
+      pomodoroCounter += 1;
+      if(pomodoroCounter % pomodorosTillLongBreak === 0) {
+        remainingTime = longBreak * 60;
+      } else {
+        remainingTime = shortBreak * 60;
+      }
+    } else {
+      remainingTime = defaultPomodoroLength * 60;
+
+    }
+    // remainingTime = isBreak ? shortBreak * 60 : defaultPomodoroLength * 60;
     updateDisplay();
   }
 }
@@ -41,7 +61,7 @@ function timerBlink() {
 // FUNCTION startCountdown
 // -----------------------------------------------------
 function startCountdown() {
-  countdownInterval = setInterval(countdown, 1000);
+  countdownInterval = setInterval(countdown, 1);
 }
 // Object eventHandler
 
@@ -69,14 +89,13 @@ function pauseCountdown() {
   if (countdownInterval) {
     clearInterval(countdownInterval);
   }
-  console.log(countdownInterval);
 }
 
 function resetCountdown() {
   pauseCountdown();
   clearBlickInterval();
   startBtn.innerHTML = "Start";
-  remainingTime = defaultMinutes * 60;
+  remainingTime = defaultPomodoroLength * 60;
   isStarted = false;
   isBreak = false;
   updateDisplay();
